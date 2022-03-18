4d3139b2d56388b371b4916b030d1cd6e86a2f33
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2022-03-18 20:53:01 -0300

    início P5

---
+++ P5/ppos_core.c
@@ -0,0 +1,237 @@
+// GRR20190372 Jorge Lucas Vicilli Jabczenski
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ucontext.h>
+
+#include "ppos.h"
+#include "ppos_data.h"
+
+int id;
+task_t *mainTask;
+task_t *taskAtual;
+task_t *dispatcher;
+
+task_t *filaTasks;
+
+//-------------------------------------------------------------------------
+void debugPrint(char *msg)
+{
+#ifdef DEBUG
+    printf("%s", msg);
+#endif
+}
+
+void *safeMalloc(int size)
+{
+    void *object = malloc(size);
+    if (!object)
+    {
+        perror("Erro no malloc");
+        exit(1);
+    }
+    return object;
+}
+//______________________________________________________________
+
+void aumentarPrioridadeDinamica(task_t *task)
+{
+    task->prioridadeDinamica += AGING;
+    if (task->prioridadeDinamica < PRIORIDADE_MAXIMA)
+    {
+        task->prioridadeDinamica = PRIORIDADE_MAXIMA;
+    }
+}
+
+void task_setprio(task_t *task, int prio)
+{
+    // Normalizando o valor da prioridade;
+    if (prio < PRIORIDADE_MAXIMA)
+        prio = PRIORIDADE_MAXIMA;
+    else if (prio > PRIORIDADE_MINIMA)
+        prio = PRIORIDADE_MINIMA;
+
+    if (task == NULL)
+    {
+        taskAtual->prioridadeEstatica = prio;
+        taskAtual->prioridadeDinamica = prio;
+    }
+    else
+    {
+        task->prioridadeEstatica = prio;
+        task->prioridadeDinamica = prio;
+    }
+}
+
+int task_getprio(task_t *task)
+{
+    if (task == NULL)
+    {
+        return taskAtual->prioridadeEstatica;
+    }
+    return task->prioridadeEstatica;
+}
+
+task_t *scheduler()
+{
+    debugPrint("inicio sched\n");
+
+    task_t *proxTask = filaTasks;
+    task_t *primeiraTask = filaTasks;
+
+    filaTasks = filaTasks->next;
+    while (primeiraTask != filaTasks)
+    {
+        if (filaTasks->prioridadeDinamica <= proxTask->prioridadeDinamica)
+        {
+            // Caso a tarefa nao seja escolhida, aumenta sua prioridade e salva a nova task escolhida
+            aumentarPrioridadeDinamica(proxTask);
+            proxTask = filaTasks;
+        }
+        else
+        {
+            aumentarPrioridadeDinamica(filaTasks);
+        }
+
+        filaTasks = filaTasks->next;
+    }
+
+    // Retorna a prioridade dinamica da task escolhida para a original
+    proxTask->prioridadeDinamica = proxTask->prioridadeEstatica;
+
+    queue_remove((queue_t **)&filaTasks, (queue_t *)proxTask);
+
+    debugPrint("fim sched\n");
+    return proxTask;
+}
+
+void dispatcherBody()
+{
+    debugPrint("inicio dispatcher\n");
+
+    while (queue_size((queue_t *)filaTasks) > 0)
+    {
+        task_t *proximaTask = scheduler();
+        if (proximaTask != NULL)
+        {
+            task_switch(proximaTask);
+            switch (proximaTask->status)
+            {
+            case PRONTA:
+                queue_append((queue_t **)&filaTasks, (queue_t *)proximaTask);
+                break;
+            case TERMINADA:
+                free(proximaTask->context.uc_stack.ss_sp);
+                break;
+            }
+        }
+    }
+
+    debugPrint("Fim da fila\n");
+    task_exit(0);
+}
+
+void task_yield()
+{
+    task_switch(dispatcher);
+}
+
+//-------------------------------------------------------------------------
+void ppos_init()
+{
+    /* desativa o buffer da saida padrao (stdout), usado pela função printf */
+    setvbuf(stdout, 0, _IONBF, 0);
+
+    debugPrint("Inicio ppos_init\n");
+
+    taskAtual = malloc(sizeof(task_t));
+    mainTask = malloc(sizeof(task_t));
+    dispatcher = malloc(sizeof(task_t));
+
+    getcontext(&(mainTask->context));
+    char *stack = safeMalloc(STACKSIZE);
+    mainTask->context.uc_stack.ss_sp = stack;
+    mainTask->context.uc_stack.ss_size = STACKSIZE;
+    mainTask->context.uc_stack.ss_flags = 0;
+    mainTask->context.uc_link = 0;
+
+    mainTask->id = id++;
+    mainTask->prev = NULL;
+    mainTask->next = NULL;
+
+    taskAtual = mainTask;
+
+    task_create(dispatcher, (void *)dispatcherBody, "Dispatcher");
+    queue_remove((queue_t **)&filaTasks, (queue_t *)dispatcher);
+
+    debugPrint("Fim ppos_init\n");
+}
+
+int task_create(task_t *task, void (*start_routine)(void *), void *arg)
+{
+    debugPrint("Inicio task_create\n");
+
+    getcontext(&(task->context));
+
+    char *stack = safeMalloc(STACKSIZE);
+
+    task->context.uc_stack.ss_sp = stack;
+    task->context.uc_stack.ss_size = STACKSIZE;
+    task->context.uc_stack.ss_flags = 0;
+    task->context.uc_link = 0;
+
+    makecontext(&(task->context), (void *)start_routine, 1, arg);
+
+    task->id = id++;
+    task->prev = NULL;
+    task->next = NULL;
+    task->status = PRONTA;
+    task->prioridadeEstatica = 0;
+    task->prioridadeDinamica = 0;
+
+#ifdef DEBUG
+    printf("task_create: %d\n", task->id);
+#endif
+
+    debugPrint("Adicionado o elemento na fila\n");
+    queue_append((queue_t **)&filaTasks, (queue_t *)task);
+
+    debugPrint("Fim task_create\n");
+    return task->id;
+}
+
+int task_switch(task_t *task)
+{
+
+#ifdef DEBUG
+    printf("task_switch: trocando contexto %d -> %d\n", taskAtual->id, task->id);
+#endif
+
+    task_t *taskAux = taskAtual;
+    taskAtual = task;
+    swapcontext(&(taskAux->context), &(task->context));
+
+    return -1;
+}
+
+void task_exit(int exit_code)
+{
+    if (taskAtual == dispatcher)
+    {
+        task_switch(mainTask);
+    }
+    else
+    {
+        taskAtual->status = TERMINADA;
+        task_switch(dispatcher);
+    }
+}
+
+int task_id()
+{
+#ifdef DEBUG
+    printf("task_id: %d\n", taskAtual->id);
+#endif
+
+    return taskAtual->id;
+}
---
+++ P5/queue.c
@@ -0,0 +1,140 @@
+// GRR20190372 Jorge Lucas Vicilli Jabczenski
+
+#include "queue.h"
+#include <stdio.h>
+
+int queue_size(queue_t *queue)
+{
+    int tamanho;
+    queue_t *primeiro = queue;
+
+    // Retorno 0 caso a lista esteja vazia
+    if (queue == NULL)
+        return 0;
+
+    // Caminha pela lista até encontrar o mesmo elemento
+    tamanho = 1;
+    while (queue->next != primeiro)
+    {
+        tamanho++;
+        queue = queue->next;
+    }
+    return tamanho;
+}
+
+int queue_append(queue_t **queue, queue_t *elem)
+{
+    if (queue == NULL)
+    {
+        fprintf(stderr, "Fila Inexistente\n");
+        return -1;
+    }
+
+    if (elem == NULL)
+    {
+        fprintf(stderr, "Elemento Inexistente\n");
+        return -2;
+    }
+
+    if (elem->prev != NULL || elem->next != NULL)
+    {
+        fprintf(stderr, "Elemento já existe em outra fila\n");
+        return -3;
+    }
+
+    if (*queue == NULL) // Caso a fila não possua nenhum elemento
+    {
+        (*queue) = elem;
+        (*queue)->prev = elem;
+        (*queue)->next = elem;
+    }
+    else
+    {
+        elem->prev = (*queue)->prev;
+        elem->next = (*queue);
+        (*queue)->prev->next = elem;
+        (*queue)->prev = elem;
+    }
+    return 0;
+}
+
+void queue_print(char *name, queue_t *queue, void print_elem(void *))
+{
+    int tamanho = queue_size(queue);
+    printf("%s: [", name);
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        print_elem((void *)queue);
+        queue = queue->next;
+        (i < tamanho - 1) ? printf(" ") : i;
+    }
+    printf("]\n");
+}
+
+int queue_remove(queue_t **queue, queue_t *elem)
+{
+#ifdef DEBUG
+    printf("chamando queue_remove\n");
+#endif
+
+    if (queue == NULL)
+    {
+        fprintf(stderr, "Fila Inexistente\n");
+        return -1;
+    }
+
+    if (elem == NULL)
+    {
+        fprintf(stderr, "Elemento Inexistente\n");
+        return -2;
+    }
+
+    if (elem->next == NULL || elem->prev == NULL)
+    {
+        fprintf(stderr, "Elemento não pertence a nenhuma fila\n");
+        return -3;
+    }
+
+    queue_t *first = (*queue);
+
+    if (queue_size(*queue) == 1 && (*queue) == (elem))
+    {
+        (*queue)->prev = NULL;
+        (*queue)->next = NULL;
+        (*queue) = NULL;
+    }
+    else
+    {
+        do
+        {
+            if ((*queue) == (elem))
+            {
+                (*queue)->prev->next = (*queue)->next;
+                (*queue)->next->prev = (*queue)->prev;
+
+                if ((*queue) == first)
+                {
+                    (*queue) = (*queue)->next;
+                }
+                else
+                {
+                    (*queue) = (first);
+                }
+
+                elem->next = NULL;
+                elem->prev = NULL;
+                return 0;
+            }
+            else
+            {
+                (*queue) = (*queue)->next;
+            }
+        } while ((*queue) != first);
+
+        // O elemento não pertencia a aquela fila
+        fprintf(stderr, "Elemento não pertencia a fila indicada\n");
+        return -3;
+    }
+    return 0;
+}
